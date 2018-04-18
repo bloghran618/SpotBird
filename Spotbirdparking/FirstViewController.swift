0b0da06dd783197b1b586c563c418b96fedc468d
user138340 <user138340@ny158.local>
2018-04-17 20:49:49 -0400

    Initial Commit

---
+++ Spotbirdparking/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Spotbirdparking
+//
+//  Created by user138340 on 4/17/18.
+//  Copyright © 2018 Spotbird. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
