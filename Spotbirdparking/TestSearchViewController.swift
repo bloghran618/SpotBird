d5ad89d30415983dfdd662c60e6e3ccf170d074a
user138340 <bloughran618@gmail.com>
2018-10-07 19:18:51 -0400

    Check why search bar cannot be clicked

---
+++ Spotbirdparking/TestSearchViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TestSearchViewController.swift
+//  Spotbirdparking
+//
+//  Created by user138340 on 10/7/18.
+//  Copyright © 2018 Spotbird. All rights reserved.
+//
+
+import UIKit
+
+class TestSearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
