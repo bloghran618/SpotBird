af5b449de1c4a0680165982a844fc060ac65d11b
user138340 <bloughran618@gmail.com>
2018-10-26 20:18:19 -0400

    Visable calendar with day picking functionality, working on month

---
+++ Spotbirdparking/CustomCell.swift
@@ -0,0 +1,13 @@
+//
+//  CustomCell.swift
+//  Spotbirdparking
+//
+//  Created by user138340 on 10/24/18.
+//  Copyright © 2018 Spotbird. All rights reserved.
+//
+
+import UIKit
+
+class CustomCell: JTAppleCell {
+    
+}
